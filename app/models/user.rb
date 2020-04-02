0ccd2791a81a9e86e56e161772a2ec38505627ac
koji0ta <tentman.programingman@gmail.com>
2020-04-02 17:22:55 +0900

    validate_change

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name, :last_kana, :first_kana, :birth, presence: true
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, 
+                                            message: "全角で入力してください" }
+
+  validates :last_kana, :first_kana, format: { with: /\A[ァ-ヶー－]+\z/ ,
+                                            message: "全角カタカナで入力してください" }
+
 end
