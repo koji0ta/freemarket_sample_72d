83320f2bc03605834ae483056733e4f2ecad811f
kyo-git <feeling.thought@gmail.com>
2020-04-03 10:39:38 +0900

    add-mypage-top-1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  before_action :contents_listing, only: [:index, :edit, :show]
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def contents_listing
+    @contents = ["メール/パスワード", "住所", "クレジットカード", "出品商品一覧"]
+    @links = []
+  end
+end
