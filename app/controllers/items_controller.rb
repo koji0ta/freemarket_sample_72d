51e8cca17e7aab4cdd3e921d2b11355fddb5a1e8
fujiimasaya <61725424+fujiimasaya@users.noreply.github.com>
2020-04-06 11:33:26 +0900

    show/testcommit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,3 +7,8 @@ class ItemsController < ApplicationController
   end
   
 end
+  def show
+    @item = Item.find(params[:id])
+  end
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :category, dependent: :destroy
   belongs_to :user
   has_many :images, dependent: :destroy
+
+
 end
