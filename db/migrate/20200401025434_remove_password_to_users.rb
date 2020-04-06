c3e1335a40fb672e5ad5e15957f646c5d6bd3c27
kyo-git <feeling.thought@gmail.com>
2020-04-06 18:32:09 +0900

    fix_migration_file

--- db/migrate/20200401025434_remove_password_to_users.rb
+++ db/migrate/20200401025434_remove_password_to_users.rb
@@ -1,6 +1,6 @@
 class RemovePasswordToUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :Users, :password, :string
-    remove_column :Users, :password_confirmation, :string
+    remove_column :users, :password, :string
+    remove_column :users, :password_confirmation, :string
   end
 end
