ba1321395f480dddf9b20cc344aeb3273fe05064
kyo-git <feeling.thought@gmail.com>
2020-04-07 16:57:45 +0900

    fix-basic-auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protect_from_forgery with: :exception
 
   protected
   def configure_permitted_parameters
@@ -20,6 +21,5 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-
 
 end
