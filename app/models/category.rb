a6cb554c60c32a022cf94c7e20943d91233e0a3d
atsushitakano <atsusi0123sp@gmail.com>
2020-04-08 13:40:38 +0900

    Merge branch 'item-new-PostFunction-develop' of https://github.com/koji0ta/freemarket_sample_72d into item-new-PostFunction-develop

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,21 +1,10 @@
-class Category < ApplicationRecord
-  has_many :items
-  
-  enum category: {
-    選択してください:0,
-    レディース:1 ,
-    メンズ:2,
-    ベビー・キッズ:3,
-    インテリア・住まい・小物:4,
-    本・音楽・ゲーム:5,
-    おもちゃ・ホビー・グッズ:6,
-    コスメ・香水・美容:7,
-    家電・スマホ・カメラ:8,
-    スポーツ・レジャー:9,
-    ハンドメイド:10,
-    チケット:11,
-    自動車・オートバイ:12,
-    その他:13
-  }
+class Category < ActiveHash::Base
+  # has_many :items
 
+  self.data = [
+    {id: 1, name: '---'}, {id: 2, name:'レディース'}, {id: 3, name: 'メンズ'},
+    {id: 4, name: 'ベビー・キッズ'}, {id: 5, name: 'インテリア・住まい・小物'}, {id: 6, name: '本・音楽・ゲーム'},
+    {id: 7, name: 'おもちゃ・ホビー・グッズ'}, {id: 8, name: '家電・スマホ・カメラ'}, {id: 9, name: 'スポーツ・レジャー'},
+    {id: 10, name: 'ハンドメイド'}, {id: 11, name: 'その他'}
+  ]
 end
