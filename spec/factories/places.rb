cbcc833299e98bb5fb8b26bbc332d97573f0277d
4ge44koyama <m220552a@gmail.com>
2020-04-09 22:24:21 +0900

    places_model_test

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
+
   factory :place do
-    
+    post_code        {"あああああああ"}
+    prefecture       {"aaaaaa"}
+    city_name        {"aaaaaa"}
+    address          {"aaaaaa"}
+    building         {"aaaaaa"}
+    user_id          {"1"}
   end
+
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Place do
+  describe '#create' do
+    # 1
+    it "post_codeとprefectureとcity_nameとaddressが存在すれば登録できること" do
+      place = build(:place)
+      expect(place).to be_valid
+    end
+  end
 end
