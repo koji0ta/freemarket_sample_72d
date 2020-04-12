57f1288f3559142f815218f19420239d32098c22
4ge44koyama <m220552a@gmail.com>
2020-04-12 21:52:02 +0900

    add_ancestory-gem-and-seeds-file

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_04_13_141820) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
