5a06f5270620f9a5e193fa154b5ff1c1454b699d
atsushitakano <atsusi0123sp@gmail.com>
2020-04-14 13:33:15 +0900

    プルリクエスト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,6 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @user = @item.user
     @category = Category.find(@item.category_id)
-    # @images = @item.images
   end
 
 
