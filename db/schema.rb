f4b5c868ed8767555868af154ecf8cacd5f29272
atsushitakano <atsusi0123sp@gmail.com>
2020-04-16 16:52:55 +0900

    done-markup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_111637) do
+ActiveRecord::Schema.define(version: 2020_04_13_141820) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
