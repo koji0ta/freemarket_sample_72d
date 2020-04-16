c8cc2333ce9e0b57e52821ffb9e0825bfc072ee7
koji0ta <tentman.programingman@gmail.com>
2020-04-16 13:36:36 +0900

    リロードしないと、適用されない状態
    途中経過として

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -64,3 +64,15 @@ $(function(){
     });
   });
 });
+
+
+$(function() {
+  $('.slider').slick({
+    prevArrow:'<i class="fa fa-angle-left arrow arrow-left"></i>',
+    nextArrow:'<i class="fa fa-angle-right arrow arrow-right"></i>',
+    autoplay:true,
+    autoplaySpeed:5000,
+    dots:true,
+  });
+});
+
