aa74f461200d285aa7a4f67820959816d0a9d44e
4ge44koyama <m220552a@gmail.com>
2020-04-20 14:16:36 +0900

    refactoring-items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,20 +44,9 @@ class ItemsController < ApplicationController
       grandchild_category = @item.category
       child_category = grandchild_category.parent
 
-      @category_parent_array = []
-      Category.where(ancestry: nil).each do |parent|
-        @category_parent_array << parent
-      end
-
-      @category_children_array = []
-      Category.where(ancestry: child_category.ancestry).each do |children|
-        @category_children_array << children
-      end
-
-      @category_grandchildren_array = []
-      Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
-        @category_grandchildren_array << grandchildren
-      end
+      @category_parent_array = Category.where(ancestry: nil)
+      @category_children_array = Category.where(ancestry: child_category.ancestry)
+      @category_grandchildren_array = Category.where(ancestry: grandchild_category.ancestry)
 
     else
       redirect_to root_path
