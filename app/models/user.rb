e556a8e6769d1b9900aa32c35f6018f3e3dfaf38
kyo-git <feeling.thought@gmail.com>
2020-04-21 13:13:17 +0900

    add-sns-login-fanction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   validates :nickname, :last_name, :first_name, :last_kana, :first_kana, :birth, presence: true
   validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, 
@@ -15,4 +15,21 @@ class User < ApplicationRecord
   has_many :cards, dependent: :destroy
   has_one :place, dependent: :destroy
   accepts_nested_attributes_for :place, allow_destroy: true
+  has_many :sns_credentials, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.omniauth :facebook,Rails.application.credentials[:FACEBOOK][:CLIENT_ID],Rails.application.credentials[:FACEBOOK][:CLIENT_SECRET]
+  config.omniauth :google_oauth2,Rails.application.credentials[:GOOGLE][:CLIENT_ID],Rails.application.credentials[:GOOGLE][:CLIENT_SECRET]
 end
